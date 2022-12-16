71d9945690e16b2cdd51c7785fe8a0cbee5ff4cf
Nine <cuz_mr.j@live.com>
2022-12-17 00:23:58 +0800

    贴边隐藏

---
+++ src/Nine.Design.WPFCollections/Nine.Design.Client/MainWindow.xaml.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using WinInterop = System.Windows.Interop;
+
+
+namespace Nine.Design.Client
+{
+    public partial class MainWindow : Window
+    {
+        #region 变量
+
+        System.IntPtr handle;//当前窗体句柄，用于win32交互
+
+        #region 贴边收缩计时器
+        private bool blnTopHide = false;
+        private bool IsMouseEnter = false;//窗体状态  true为显示 false为隐藏
+        private System.Windows.Forms.Timer timer;//计时器  通过win32api实时获取鼠标位置
+        #endregion
+
+        #region 最大化、最小化、关闭、图片资源
+        readonly BitmapImage Exit_Enter = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/close_696969.png", UriKind.RelativeOrAbsolute));
+        readonly BitmapImage Exit_Leave = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/close_9ea0a2.png", UriKind.RelativeOrAbsolute));
+        readonly BitmapImage Max_Enter = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/max_696969.png", UriKind.RelativeOrAbsolute));
+        readonly BitmapImage Max_Leave = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/max_9ea0a2.png", UriKind.RelativeOrAbsolute));
+        readonly BitmapImage Mini_Enter = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/mini_696969.png", UriKind.RelativeOrAbsolute));
+        readonly BitmapImage Mini_Leave = new BitmapImage(new Uri("pack://application:,,,/Nine.Design.Resource;component/images/btn/mini_9ea0a2.png", UriKind.RelativeOrAbsolute));
+        #endregion
+
+        #endregion
+
+        #region 构造方法
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //引發此事件以支援與 Win32 的交互操作
+            //最大化不遮挡任务栏
+            this.SourceInitialized += new EventHandler(CustomWindowsSourceInitialized);
+
+            #region 贴边收缩计时器
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 300;
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+            #endregion
+
+            #region 关闭贴边放大，WindowStyle="None" 设置为none
+            this.PreviewMouseLeftButtonDown += (sender, e) =>
+            {
+                if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                {
+                    this.ResizeMode = ResizeMode.NoResize;
+                }
+            };
+            this.PreviewMouseLeftButtonUp += (sender, e) =>
+            {
+                if (this.ResizeMode == ResizeMode.NoResize)
+                {
+                    this.ResizeMode = ResizeMode.CanResize;
+                }
+            };
+            #endregion
+
+            #region 设置窗口屏幕居中置顶
+            Top = 0;
+            Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+            #endregion
+        }
+        #endregion
+
+        #region 最大化不遮挡任务栏
+        public void CustomWindowsSourceInitialized(object sender, EventArgs e)
+        {
+            handle = (new WinInterop.WindowInteropHelper(this)).Handle;
+            WinInterop.HwndSource.FromHwnd(handle).AddHook(new WinInterop.HwndSourceHook(Win32Interactive.WindowProc));
+        }
+        #endregion
+
+        #region 加载事件
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //隐藏任务栏图标
+            Win32Interactive.SetWindowLong(handle, -20, (IntPtr)0x8000000);
+        }
+        #endregion
+
+        #region 关闭事件
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Process[] myproc = Process.GetProcesses();
+            foreach (Process item in myproc)
+            {
+                if (item.ProcessName == "Nine.Design.Client")
+                {
+                    item.Kill();
+                }
+            }
+        }
+        #endregion
+
+        #region 窗口置顶
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+            Console.WriteLine("工具条窗口置顶");
+        }
+        #endregion
+
+        #region 鼠标点击拖动窗体
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+        #endregion
+
+        #region 贴边收缩计时器实现
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            Point point = PointToScreen(Mouse.GetPosition(this));//获取鼠标相对桌面的位置
+            if (point.X != 0 || point.Y != 0)
+            {
+                IsMouseEnter = true;    //鼠标在窗体内部
+            }
+            else if (point.X == 0 && point.Y == 0)
+            {
+                IsMouseEnter = false;   //鼠标离开窗体
+            }
+            if (this.Top <= 0)
+            {
+                if (blnTopHide)
+                {
+                    if (this.Top <= 0 && IsMouseEnter)
+                    {
+                        while (this.Top < -2)
+                            this.Top++;
+                        blnTopHide = false;
+                    }
+                }
+                else
+                {
+                    if (this.Top <= 0 && !IsMouseEnter)
+                    {
+                        while (this.Top > -(this.Height - 12))
+                            this.Top--;
+                        blnTopHide = true;
+                    }
+                    return;
+                }
+            }
+            else if (this.Left <= 0)
+            {
+                if (blnTopHide)
+                {
+                    Point b = Mouse.GetPosition(this);//获取鼠标相对窗口的位置
+                    if (this.Left < 200 && IsMouseEnter)
+                    {
+                        while (this.Left < -2)
+                            this.Left++;
+                        blnTopHide = false;
+                    }
+                }
+                else
+                {
+                    Point b = Mouse.GetPosition(this);//获取鼠标相对窗口的位置
+                    if (this.Left < 5 && !IsMouseEnter)
+                    {
+                        while (this.Left > -(this.Width - 12))
+                            this.Left--;
+                        blnTopHide = true;
+                    }
+                    return;
+                }
+            }
+            else if (this.Left + this.Width >= Win32Interactive.转换为DPI参数(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width))
+            {
+                var s = Win32Interactive.转换为DPI参数(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+                if (blnTopHide)
+                {
+                    if (point.Y >= this.Top && point.Y <= this.Top + this.Height && IsMouseEnter)
+                    {
+                        while (this.Left > Win32Interactive.转换为DPI参数(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width) - (this.Width - 1))
+                            this.Left--;
+                        blnTopHide = false;
+                    }
+                }
+                else
+                {
+                    Point b = Mouse.GetPosition(this);//获取鼠标相对窗口的位置
+                    if (this.Left + this.Width >= Win32Interactive.转换为DPI参数(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width) && !IsMouseEnter)
+                    {
+                        while (this.Left < Win32Interactive.转换为DPI参数(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width) - 12)
+                            this.Left++;
+                        blnTopHide = true;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region 最小化
+        private void Mini_Img_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Mini_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Mini_Img.Source = Mini_Enter;
+        }
+        private void Mini_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Mini_Img.Source = Mini_Leave;
+        }
+        #endregion
+
+        #region 最大化
+        private void Max_Img_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                Storyboard storyboard = StartGrid.FindResource("StartTo") as Storyboard;
+                //DoubleAnimation DoubleAnimation= storyboard.Children[0] as DoubleAnimation;
+                Thickness myThickness = new Thickness
+                {
+                    Bottom = 10,
+                    Left = 10,
+                    Right = 10,
+                    Top = 10
+                };
+                StartGrid.Margin = myThickness;
+                (storyboard.Children[0] as DoubleAnimation).To = 300;
+                storyboard.Begin(this);
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                Thickness myThickness = new Thickness
+                {
+                    Bottom = 0,
+                    Left = 0,
+                    Right = 0,
+                    Top = 0
+                };
+                StartGrid.Margin = myThickness;
+                Screen[] screens = Screen.AllScreens;
+                Storyboard storyboard = StartGrid.FindResource("StartTo") as Storyboard;
+                if (screens.Length >= 1)
+                {
+                    IntPtr handle = new WindowInteropHelper(this).Handle;
+                    var currScreen = System.Windows.Forms.Screen.FromHandle(handle);
+                    if (currScreen.Primary)
+                    {
+                        (storyboard.Children[0] as DoubleAnimation).To = SystemParameters.WorkArea.Width;
+                    }
+                    else
+                    {
+                        (storyboard.Children[0] as DoubleAnimation).To = currScreen.WorkingArea.Width;
+                    }
+                }
+                //DoubleAnimation DoubleAnimation = storyboard.Children[0] as DoubleAnimation;
+                //double x = SystemParameters.WorkArea.Width;//得到屏幕工作区域宽度
+                //double y = SystemParameters.WorkArea.Height;//得到屏幕工作区域高度
+                //double x1 = SystemParameters.PrimaryScreenWidth;//得到屏幕整体宽度
+                //double y1 = SystemParameters.PrimaryScreenHeight;//得到屏幕整体高度
+
+                //TimeSpan timeSpan = new TimeSpan(20000);
+
+                //Duration duration = new Duration(timeSpan);
+                //(storyboard.Children[0] as DoubleAnimation).Duration = duration;
+
+
+                storyboard.Begin(this);
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+        private void Max_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Max_Img.Source = Max_Enter;
+        }
+        private void Max_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Max_Img.Source = Max_Leave;
+        }
+        #endregion
+
+        #region 关闭
+        private void Exit_Img_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Process[] myproc = Process.GetProcesses();
+            foreach (Process item in myproc)
+            {
+                if (item.ProcessName == "Nine.Design.Client")
+                {
+                    item.Kill();
+                }
+            }
+        }
+        private void Exit_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Exit_Img.Source = Exit_Enter;
+        }
+        private void Exit_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Exit_Img.Source = Exit_Leave;
+        }
+        #endregion
+
+        #region 输出文本框文字滚动到最后
+        private void ConsoleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ConsoleTextBox.ScrollToEnd();
+        }
+        #endregion
+    }
+}
---
+++ src/Nine.Design.WPFCollections/Nine.Design.Client/Win32Interactive.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WinInterop = System.Windows.Interop;
+
+namespace Nine.Design.Client
+{
+    public static class Win32Interactive
+    {
+        #region 最大化不遮挡任务栏
+        public static System.IntPtr WindowProc(
+              System.IntPtr hwnd,
+              int msg,
+              System.IntPtr wParam,
+              System.IntPtr lParam,
+              ref bool handled)
+        {
+            switch (msg)
+            {
+                case 0x0024:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+
+            return (System.IntPtr)0;
+        }
+
+        private static void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != System.IntPtr.Zero)
+            {
+
+                MONITORINFO monitorInfo = new MONITORINFO();
+                GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        /// <summary>
+        /// POINT aka POINTAPI
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            /// <summary>
+            /// x coordinate of point.
+            /// </summary>
+            public int x;
+            /// <summary>
+            /// y coordinate of point.
+            /// </summary>
+            public int y;
+
+            /// <summary>
+            /// Construct a point of coordinates (x,y).
+            /// </summary>
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        /// <summary>
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {
+            /// <summary>
+            /// </summary>            
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+
+            /// <summary>
+            /// </summary>            
+            public RECT rcMonitor = new RECT();
+
+            /// <summary>
+            /// </summary>            
+            public RECT rcWork = new RECT();
+
+            /// <summary>
+            /// </summary>            
+            public int dwFlags = 0;
+        }
+
+        /// <summary> Win32 </summary>
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            /// <summary> Win32 </summary>
+            public int left;
+            /// <summary> Win32 </summary>
+            public int top;
+            /// <summary> Win32 </summary>
+            public int right;
+            /// <summary> Win32 </summary>
+            public int bottom;
+
+            /// <summary> Win32 </summary>
+            public static readonly RECT Empty = new RECT();
+
+            /// <summary> Win32 </summary>
+            public int Width
+            {
+                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+            }
+            /// <summary> Win32 </summary>
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+
+            /// <summary> Win32 </summary>
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+
+            /// <summary> Win32 </summary>
+            public RECT(RECT rcSrc)
+            {
+                this.left = rcSrc.left;
+                this.top = rcSrc.top;
+                this.right = rcSrc.right;
+                this.bottom = rcSrc.bottom;
+            }
+
+            /// <summary> Win32 </summary>
+            public bool IsEmpty
+            {
+                get
+                {
+                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
+                    return left >= right || top >= bottom;
+                }
+            }
+            /// <summary> Return a user friendly representation of this struct </summary>
+            public override string ToString()
+            {
+                if (this == RECT.Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+
+            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+
+            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
+            public override int GetHashCode()
+            {
+                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            }
+
+
+            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
+            public static bool operator ==(RECT rect1, RECT rect2)
+            {
+                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+            }
+
+            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
+            public static bool operator !=(RECT rect1, RECT rect2)
+            {
+                return !(rect1 == rect2);
+            }
+        }
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DllImport("User32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+        #endregion
+
+        #region 隐藏任务栏图标
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nindex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern UInt32 GetWindowLong(IntPtr hWnd, int index);
+        #endregion
+
+        #region DPI转换
+        [DllImport("User32.dll")] private static extern IntPtr GetDC(HandleRef hWnd);
+        [DllImport("User32.dll")] private static extern int ReleaseDC(HandleRef hWnd, HandleRef hDC);
+        [DllImport("GDI32.dll")] private static extern int GetDeviceCaps(HandleRef hDC, int nIndex);
+        private static int _dpi = 0;
+        public static int DPI
+        {
+            get
+            {
+                if (_dpi == 0)
+                {
+                    HandleRef desktopHwnd = new HandleRef(null, IntPtr.Zero);
+                    HandleRef desktopDC = new HandleRef(null, GetDC(desktopHwnd));
+                    _dpi = GetDeviceCaps(desktopDC, 88 /*LOGPIXELSX*/);
+                    ReleaseDC(desktopHwnd, desktopDC);
+                }
+                return _dpi;
+            }
+        }
+
+        public static double 转换为DPI参数(double pixels)
+        {
+            return (double)pixels * 96 / DPI;
+        }
+
+        public static double 转换为实际参数(double pixels)
+        {
+            return pixels * DPI / 96;
+        }
+
+        public static Point 转换为DPI坐标(Point pixels)
+        {
+            return new Point((double)pixels.X * 96 / DPI, (double)pixels.Y * 96 / DPI);
+        }
+        public static Point 转换为实际坐标(Point pixels)
+        {
+
+            return new Point((double)pixels.X * DPI / 96, (double)pixels.Y * DPI / 96);
+        }
+
+        public static System.Windows.Size 转换为DPI尺寸(System.Windows.Size pixels, double dpi = 0)
+        {
+            if (dpi == 0)
+            {
+                dpi = DPI;
+            }
+            return new System.Windows.Size((double)pixels.Width * 96 / dpi, (double)pixels.Height * 96 / dpi);
+        }
+        public static System.Windows.Size 转换为实际尺寸(System.Windows.Size pixels, double dpi = 0)
+        {
+            if (dpi == 0)
+            {
+                dpi = DPI;
+            }
+            return new System.Windows.Size((double)pixels.Width * dpi / 96, (double)pixels.Height * dpi / 96);
+        }
+
+        public static T? 实际参数转为DPI参数<T>(T 泛型参数)
+        {
+            if (泛型参数 is double)
+            {
+                double 实例参数 = (dynamic)泛型参数;
+                实例参数 = 实例参数 * 96 / DPI;//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型            
+                return 返回结果;
+            }
+            else if (泛型参数 is System.Windows.Point)
+            {
+                System.Windows.Point 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new System.Windows.Point(实例参数.X * 96 / DPI, 实例参数.Y * 96 / DPI);//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is System.Windows.Size)
+            {
+                System.Windows.Size 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new System.Windows.Size(实例参数.Width * 96 / DPI, 实例参数.Height * 96 / DPI);//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is Point)
+            {
+                Point 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new Point(实例参数.X * 96 / DPI, 实例参数.Y * 96 / DPI);//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is Thickness)
+            {
+                Thickness 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new Thickness(实例参数.Left * 96 / DPI, 实例参数.Top * 96 / DPI, 实例参数.Right * 96 / DPI, 实例参数.Bottom * 96 / DPI);//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+        public static T? DPI参数转为实际参数<T>(T 泛型参数)
+        {
+            if (泛型参数 is double)
+            {
+                T 返回结果 = 转换为实际参数((dynamic)泛型参数); //转泛型            
+                return 返回结果;
+            }
+            else if (泛型参数 is System.Windows.Point)
+            {
+                System.Windows.Point 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new System.Windows.Point(转换为实际参数(实例参数.X), 转换为实际参数(实例参数.Y));//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is System.Windows.Size)
+            {
+                System.Windows.Size 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new System.Windows.Size(转换为实际参数(实例参数.Width), 转换为实际参数(实例参数.Height));//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is Point)
+            {
+                Point 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new Point(转换为实际参数(实例参数.X), 转换为实际参数(实例参数.Y));//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else if (泛型参数 is Thickness)
+            {
+                Thickness 实例参数 = (dynamic)泛型参数;//拆箱
+                实例参数 = new Thickness(转换为实际参数(实例参数.Left), 转换为实际参数(实例参数.Top), 转换为实际参数(实例参数.Right), 转换为实际参数(实例参数.Bottom));//计算
+                T 返回结果 = (dynamic)实例参数; //转泛型        
+                return 返回结果;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+        #endregion
+    }
+}
